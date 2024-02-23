a4e803e5fa4a297f84060e52f8a4c184423ed8f9
ertai_b03@mail.ru <95402990+BEkasss11@users.noreply.github.com>
2024-02-23 10:36:36 +0600

    first commit

---
+++ mymodule/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/lib/pq"
+)
+
+func main() {
+    const (
+        host     = "localhost"
+        port     =  5432
+        user     = "postgres"
+        password = "your-password"
+        dbname   = "calhounio_demo"
+      )
+      db , err := sql.Open("host = localhost " , pq)
+}
